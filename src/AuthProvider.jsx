36113c827e3b6dde5c67b5e52b42e03cd1ed0e08
Nafis <bhuiyannafis517@gmail.com>
2023-11-30 23:08:15 +0600

    similar

--- src/AuthProvider.jsx
+++ src/AuthProvider.jsx
@@ -1,55 +1,50 @@
 import { createContext, useEffect, useState } from "react";
-import { GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut } from "firebase/auth";
+import { GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut } from "firebase/auth"
 import app from "./firebase.config";
-
-
-
 
 export const AuthContext = createContext(null)
 const AuthProvider = ({children}) => {
-    const [user,setUser] = useState(null)
+    const [userFinal,setuserFinal] = useState(null)
     const [loading,setLoading] = useState(true)
 
     const auth = getAuth(app)
     const provider = new GoogleAuthProvider()
     const createUser = (email,password) =>{
-       return createUserWithEmailAndPassword(auth,email,password)
-    }
-    const SignIn = (email,password) => {
-      return  signInWithEmailAndPassword(auth,email,password)
+        setLoading(true)
+        return createUserWithEmailAndPassword(auth,email,password)
     }
 
-    const GoogleSignIn = () =>{
+    const logIn = (email,password) =>{
+        setLoading(true)
+
+        return signInWithEmailAndPassword(auth,email,password)
+    }
+
+    const signInWithGoogle = () =>{
         return signInWithPopup(auth,provider)
     }
 
-    const Logout = () =>{
+    const LogOut = () =>{
+        setLoading(true)
+
         return signOut(auth)
     }
-
- 
 
 
     useEffect(()=>{
         const unSubscribe = onAuthStateChanged(auth,currentUser =>{
             console.log(currentUser)
-            setUser(currentUser)
+            setuserFinal(currentUser)
             setLoading(false)
 
         })
-        return unSubscribe()
+        return unSubscribe
        
     },[])
-
-
-    const AuthInfo = {
-        user,loading,
-        createUser,SignIn,GoogleSignIn,Logout
-    }
-   
+    const AuthInfo = {userFinal,loading,createUser,logIn,signInWithGoogle,LogOut}
     return (
         <AuthContext.Provider value={AuthInfo}>
-            {children}
+          {children}
         </AuthContext.Provider>
     );
 };
--- src/Login.jsx
+++ src/Login.jsx
@@ -1,81 +1,101 @@
 import { useContext, useState } from "react";
-import { Link } from "react-router-dom";
 import { AuthContext } from "./AuthProvider";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import Swal from "sweetalert2";
 
 const Login = () => {
-    const {SignIn,GoogleSignIn} = useContext(AuthContext)
-    const [error,setError] = useState()
+    const {logIn,signInWithGoogle} = useContext(AuthContext)
+    const [error,seterror] = useState('')
+    const location = useLocation()
+    const navigate = useNavigate()
+
+
+    const form = location.state?.from?.pathname || "/"
+    console.log(form)
+console.log(error)
+
     const handleLogin = e =>{
         e.preventDefault()
-        const form = new FormData(e.currentTarget)
-        const email = form.get('email')
+              const form = new FormData(e.currentTarget)
+        const email = form.get('Email')
         const password = form.get('password')
         console.log(email,password)
-        SignIn(email,password)
-        .then(res=>{
-          Swal.fire(
-            'Well Done!',
-                     'You have successfully Login!',
-                     'success'
-          )
-          setError = null
+            logIn(email,password)
+            .then(res => {
+                const user = res.user
+                if(user){
+                    Swal.fire(
+                        'Good job!',
+                      'You have successfully Login!',
+                      'success')
+                }
+                navigate(form, {replace: true});
+                console.log(user)
+            })
+            .catch(error =>{
+                seterror(error.message)
+            })
+        
+   
+       
 
-            console.log(res.user)
-        })
-        .catch(error =>{
-            setError(error.message)
-        })
     }
-
-
-    const handlegoogle = () =>{
-        GoogleSignIn()
+    const handleGoogle = () =>{
+      signInWithGoogle()
         .then(res =>{
-            console.log(res.user)
-        })
-        .catch(error =>{
+           const user = res.user
+           console.log(user)
+           if(user){
+            Swal.fire(
+                'Good job!',
+              'You have successfully Login with social!',
+              'success')
+
+           }
+          //  navigate('/')
             console.log(user)
         })
+        .catch(error =>{
+            seterror(error.message)
+        })
     }
-
     return (
         <div>
             <div className="hero min-h-screen bg-base-200">
-                <div className="hero-content flex-col">
-                  <div className="text-center lg:text-left">
-                    <h1 className="text-5xl font-bold">Login now!</h1>
-                  </div>
-                  <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-                    <form className="card-body" onSubmit={handleLogin}>
-                      <div className="form-control">
-                        <label className="label">
-                          <span className="label-text">Email</span>
-                        </label>
-                        <input type="email" name="email" placeholder="email" className="input input-bordered" required />
-                      </div>
-                      <div className="form-control">
-                        <label className="label">
-                          <span className="label-text">Password</span>
-                        </label>
-                        <input type="password" name="password" placeholder="password" className="input input-bordered" required />
-                        <label className="label">
-                          <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
-                        </label>
-                      </div>
-                      <div className="form-control mt-6">
-                        <button className="btn btn-primary">Login</button>
-                      </div>
-                    </form>
-                    <p className="text-red-700">{error}</p>
-                    <div className="mx-3 mb-4">
-                    <p>Don't have account ? please <Link to="/register" className="link-success">Register</Link></p>
-                    <button onClick={handlegoogle} className="btn btn-accent w-full mt-3">Continue with Google</button>
-
-                    </div>
-                  </div>
-                </div>
-              </div>
+  <div className="hero-content flex-col lg:flex">
+    <div className="text-center lg:text-left">
+      <h1 className="text-5xl font-bold">Login now!</h1>
+    </div>
+   
+   <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+      <form onSubmit={handleLogin}  className="card-body">
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Email</span>
+          </label>
+          <input type="email" name='Email' required placeholder="email" className="input input-bordered" required />
+        </div>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Password</span>
+          </label>
+          <input type="password" name="password" required placeholder="password" className="input input-bordered" required />
+          <label className="label">
+            <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+          </label>
+        </div>
+        <div className="form-control mt-6">
+          <h1 ><button  className="btn btn-accent w-full">Login</button></h1>
+          <p>{error}</p>
+        </div>
+      </form>
+      <p className=" mb-3 text-center">Don't have account ?<Link to="/register" className="link link-accent">Register</Link></p>
+      <h1 className="text-center mb-2 font-medium">Or,</h1>
+      <button onClick={handleGoogle} className=" btn btn-active hover:bg-green-600 mx-2">  Continue with Google</button>
+    </div>
+   
+  </div>
+</div>
         </div>
     );
 };
